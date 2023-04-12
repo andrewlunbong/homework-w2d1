5de9db007623dfc87b00c3731396458bb0145570
andrewlunbong <andrewlunbong@gmail.com>
2023-04-12 10:02:41 +0100

    moved .git to see full project

--- team_class/src/team.py
+++ team_class/src/team.py
@@ -3,12 +3,18 @@ class Team:
         self.name = input_name
         self.players = input_players
         self.coach = input_coach
+        self.point = 0
 
     def add_player(self, input_new_player):
         self.players.append(input_new_player)
 
-    def has_player(self, has_player):
-        return has_player in self.players
+    def has_player(self, player):
+        return self.players.count(player)>0
+        #return player in self.players
+
+    def play_game(self, game_won):
+        if game_won:
+            self.point += 3    
 
 
 
